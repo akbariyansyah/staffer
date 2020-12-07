37c087077bf7b88f4553ad2e4aa6f6efe02580f3
akbariyansyah <riyansean21366@gmail.com>
2020-12-07 16:45:25 +0700

    REFACORING

--- backend/api/position/position_model.go
+++
@@ -1,8 +0,0 @@
-package position
-
-type Position struct {
-	ID       string `json:"id"`
-	Code     string `json:"code" validate:"required"`
-	Name     string `json:"name" validate:"required"`
-	IsDelete string `json:"is_delete"`
-}
